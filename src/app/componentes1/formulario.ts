d32dd8f4e62a33614f02a4cdc50e592cb34acbef
sergio <sergiovergaravega@outlook.com>
2024-04-21 23:01:08 -0500

    Avances desarrollo de la historia de usuario HU02 en frontend

---
+++ src/app/componentes1/formulario.ts
@@ -0,0 +1,10 @@
+export interface Formulario {
+  serial: number;
+  nombre: string;
+  apellido: string ;
+  celular: number ;
+  correo: string ;
+  documento: number ;
+  salario: number ;
+  gastos: number ;
+}
