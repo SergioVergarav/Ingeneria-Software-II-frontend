39f978a0d51d55f57ae3f239dfff4562a52889bc
sergio <sergiovergaravega@outlook.com>
2024-04-21 23:02:12 -0500

    Avances desarrollo de la historia de usuario HU02 en frontend

--- src/app/componentes1/services/formulario.service.ts
+++ src/app/componentes1/services/formulario.service.ts
@@ -7,7 +7,7 @@ import {Formulario} from "../../componentes1/formulario";
 })
 export class FormularioService {
 
-  rutaformularios = 'http://localhost:8862/api/formulario/'
+  rutaformulario = 'http://localhost:8862/api/formulario/'
 
   private httpClient = inject(HttpClient)
   constructor() { }
