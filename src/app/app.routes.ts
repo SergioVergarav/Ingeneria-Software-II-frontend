ad9c7551c8a6982537e924eabefa59b2572a3982
sergio <sergiovergaravega@outlook.com>
2024-05-15 22:49:45 -0500

    Avance desarrollo Hu01

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,15 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'solicitudes',
     loadComponent: () => import('./componentes/solicitudes/solicitudes.component')
   },
   {
     path: 'registro',
     loadComponent: () => import('./componentes/solicitudesform/solicitudesform.component')
+  },
+  {
+    path: '',
+    loadComponent: () => import('./componentes/inicio/inicio.component')
   }
 ];
--- src/app/componentes/inicio/inicio.component.ts
+++ src/app/componentes/inicio/inicio.component.ts
@@ -7,6 +7,6 @@ import { Component } from '@angular/core';
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
-export class InicioComponent {
+export default class InicioComponent {
 
 }
